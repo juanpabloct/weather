c219a77eebd5e775ad3ca505b6579957158b76a9
juanpabloct <juanptorres2003@gmail.com>
2023-02-10 08:04:42 -0500

    component of Header

---
+++ src/components/header.tsx
@@ -0,0 +1,7 @@
+export const Header = () => {
+  return (
+    <header className="text-2xl to-slate-500 from-slate-700 w-full bg-gradient-to-b  text-white ">
+      <h1 className="text-center py-2 font-semibold">The Weather</h1>
+    </header>
+  );
+};
