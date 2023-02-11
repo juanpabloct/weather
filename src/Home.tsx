13dc122d388367d2fb956060289c795c455c143c
juanpabloct <juanptorres2003@gmail.com>
2023-02-11 10:11:27 -0500

    add props necesary

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,19 +1,41 @@
 import { Grid } from "@mui/material";
 import { useState } from "react";
 import { Header } from "./components/header";
+import { InformationCity } from "./components/informationCity";
 import { SearchCity } from "./components/searchCity";
+import { InfoCityData } from "./types/infoCityData";
 function Home() {
-  const [city, setCity] = useState("");
-  console.log(city);
+  const [error, setError] = useState("");
+  const [InfoCity, setInfoCity] = useState<InfoCityData>(Object.create(null));
+  const [units, setUnits] = useState("default");
+
   return (
-    <>
+    <div className="flex items-center justify-center flex-col">
       <Header />
-      <div>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+        }}
+      >
         <Grid container columns={{ xs: 12 }}>
-          <SearchCity city={city} setCity={setCity} />
+          <SearchCity
+            SetInfoCity={setInfoCity}
+            setError={setError}
+            error={error}
+            units={units}
+          />
         </Grid>
-      </div>
-    </>
+      </form>
+      {error.length > 0 ? (
+        <h3 className="text-center text-red-400 text-sm font-semibold">
+          Error: {error}
+        </h3>
+      ) : (
+        InfoCity.name && (
+          <InformationCity information={InfoCity} setUnits={setUnits} />
+        )
+      )}
+    </div>
   );
 }
 
