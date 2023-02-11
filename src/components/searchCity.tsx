4f0867de70d616da95c1180e42bf6a5223212c71
juanpabloct <juanptorres2003@gmail.com>
2023-02-11 10:10:17 -0500

    Acciones al darle enter o salir del formulario

--- src/components/searchCity.tsx
+++ src/components/searchCity.tsx
@@ -1,7 +1,27 @@
-import { Grid, TextField } from "@mui/material";
-import { CityProps } from "../types";
+import { Button, Grid, TextField } from "@mui/material";
+import { Dispatch, useEffect, useState } from "react";
+import { Url } from "../conection/url";
+import { ErrorCity } from "../types/errorResponse";
+import { InfoCityData } from "../types/infoCityData";
 
-export const SearchCity = ({ city, setCity }: CityProps) => {
+export const SearchCity = ({
+  SetInfoCity,
+  setError,
+  error,
+  units,
+}: {
+  SetInfoCity: Dispatch<React.SetStateAction<InfoCityData>>;
+  setError: Dispatch<React.SetStateAction<string>>;
+  error: string;
+  units: string;
+}) => {
+  const [city, setCity] = useState("");
+  useEffect(() => {
+    city.length > 0 &&
+      Url({ city, units }).then((res) => {
+        SetInfoCity(res.data);
+      });
+  }, [units]);
   return (
     <Grid
       container
@@ -11,7 +31,7 @@ export const SearchCity = ({ city, setCity }: CityProps) => {
       justifyContent={"center"}
     >
       <div>
-        <h3 className="text-center mt-5 font-medium text-base">
+        <h3 className="text-center mt-5 font-medium text-base my-2">
           Write the City to Search
         </h3>
         <TextField
@@ -20,6 +40,27 @@ export const SearchCity = ({ city, setCity }: CityProps) => {
           variant="outlined"
           onChange={({ target }) => {
             setCity(target.value);
+          }}
+          onBlur={async ({ target }) => {
+            setCity(target.value);
+            try {
+              const { data } = await Url({ city: target.value, units: units });
+              SetInfoCity(data);
+              error.length > 0 && setError("");
+            } catch (error: any) {
+              setError(error.response.data.message);
+            }
+          }}
+          onKeyDown={async ({ key }) => {
+            if (key === "Enter") {
+              try {
+                const { data } = await Url({ city: city, units });
+                SetInfoCity(data);
+                error.length > 0 && setError("");
+              } catch (error: any) {
+                setError(error.response.data.message);
+              }
+            }
           }}
         />
       </div>
